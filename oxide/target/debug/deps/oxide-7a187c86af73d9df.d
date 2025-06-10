2894d066ec5bcf3347e6252f02dbab388806940e
davidonut3 <david@martinengeanne.nl>
2025-06-10 12:14:48 +0200

    Added .gitignore

--- oxide/target/debug/deps/oxide-7a187c86af73d9df.d
+++
@@ -1,5 +0,0 @@
-C:\Users\spele\source\repos\ChemGame\oxide\target\debug\deps\liboxide-7a187c86af73d9df.rmeta: src\lib.rs
-
-C:\Users\spele\source\repos\ChemGame\oxide\target\debug\deps\oxide-7a187c86af73d9df.d: src\lib.rs
-
-src\lib.rs:
--- oxide/target/debug/deps/oxide-867e882d4a4a0c79.d
+++
@@ -1,5 +0,0 @@
-C:\Users\spele\source\repos\ChemGame\oxide\target\debug\deps\liboxide-867e882d4a4a0c79.rmeta: src\lib.rs
-
-C:\Users\spele\source\repos\ChemGame\oxide\target\debug\deps\oxide-867e882d4a4a0c79.d: src\lib.rs
-
-src\lib.rs:
